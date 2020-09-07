59cce42588cb7951baae7b317eb82112c41af1f4
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-09-07 13:12:47 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+from Fe import *
+
+def name():
+    return "vishwak"
+
+def on(val):
+    return "on " + str(val) 
+
+def off(val):
+    return "off " + str(val)
+
+jS = {
+    "functions":["name","hello","on","off"],
+    "name":{
+        "name":name,
+        "argument":[]
+        },
+    "on":{
+        "name":on,
+        "argument":["light","fan","motor"]
+        },
+    "off":{
+        "name":off,
+        "argument":["light","fan","motor"]
+        }
+    }
+
+obj = functionExecutor()
+print(obj.textParsing("what is our name ",jS))
