fdf3422fbfabaf71a5b0db36f31999343cf57e51
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-10-19 13:56:16 +0530

    Update Fe.js

--- js/Fe.js
+++ js/Fe.js
@@ -1,23 +1,58 @@
-var functionExecutor = {
-functionCall : function(methods = 0,arguments = 0) {
-try{
-if(arguments == 0 || methods == 0){return methods();}
-else{return methods(arguments);}
+//example functions
+function on(){
+	return "light on";
 }
-catch(err){
-return "Exception";
+
+function off(){
+	return "light off";
 }
-},
-textParsing : function(text=0,jsonObject=0){
-try{
-if(text == 0 || jsonObject == 0){return "Exception"}
-else{var setA = text.toString().split(" ");
-var funcExtract = jsonObject["function"].filter(x => setA.includes(x));
-if(funcExtract.length <= 0){return "No function";}
-else{return this.functionCall(jsonObject[funcExtract[0]]["name"])}
+
+//it is used by fn class to find the function inside string. 
+const jsonObject = {
+	"function":["on","off"],
+	"on":{
+		"name":"on",
+		"argument":[]
+	},
+	"off":{
+		"name":"off",
+		"argument":[]
+	}
 }
+
+//used to execute function found by fn class.
+function ex(funcName){
+	if(funcName == "on"){
+		return on();
+		}
+	if(funcName == "off"){
+		return off();
+		}
+	else{
+		return "error:2"
+		}
 }
-catch(err){
-return "Exception";}
-}
+
+//used to find the function inside string
+var fn = {
+  parse : function(text=0){
+    try{
+      if(text == 0){
+        return "err:0"
+      }
+      else{
+        var setA = text.toString().split(" ");
+        var funcExtract = jsonObject["function"].filter(x => setA.includes(x));
+        if(funcExtract.length <= 0){
+          return "err:1";
+        }
+        else{
+          return ex(jsonObject[funcExtract[0]]["name"]);
+        }
+      }
+    }
+    catch(err){
+      return "err:0";
+    }
+  }
 };
